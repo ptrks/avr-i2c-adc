30b3588be533bece6e8557d612e14b03a67f5fbf
Patrick Servello <patrick.servello@gmail.com>
2015-02-01 13:58:46 +0000

    tidying up

--- lib/PCF8591/pcf8591.h
+++ lib/PCF8591/pcf8591.h
@@ -2,8 +2,6 @@
 #define PCF8591_H
 
 #include <stdint.h>
-
-
 /* Available analog channels on PCF8591 */
 enum {
 	AI_CH_0 = 0,
@@ -12,17 +10,11 @@ enum {
 	AI_CH_3
 };
 
-
-
 /* Initilizes all required TWI hardware for PCF8591 */ 
 void pcf8591_init(void);
-
 /* Read a value from one of the PCF8591 analog input channels */
 uint8_t pcf8591_analog_read(uint8_t channel);
-
 /* Write a value to the single PCF8591 analog output channel */
 void pcf8591_analog_write(uint8_t value);
-
-
 
 #endif
