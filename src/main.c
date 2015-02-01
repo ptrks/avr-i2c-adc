0b8b8b82bf7ff1fa05edfa6aebcb4d7536758f8e
Patrick Servello <patrick.servello@gmail.com>
2015-02-01 13:11:39 +0000

    updated formatting

--- src/main.c
+++ src/main.c
@@ -1,37 +1,18 @@
 
-/* 	
-
-	Example of PCF8591 ADC/DAC usage with an Atmega328p running at 16 MHz. Serial output is piped to stdout
+/* Example of PCF8591 ADC/DAC usage with an Atmega328p running at 16 MHz. Serial output is piped to stdout
     for easy formatted printing using printf. 
-
 */
-
 #include <avr/io.h>
 #include <stdint.h>
 #include <util/delay.h>
 #include "serial.h"
 #include "pcf8591.h"
 
+void mcu_init (void);
 
-
-/* USART and TWI initialize */ 
-void mcu_init(void) {
-	usart_initialize();
-	stdout = &uart_output;
-	_delay_ms(10);
-	pcf8591_init();
-	printf("PCF8591 initialized.....\n");
-}
-
-
-
-int main(void)
-{
-
-	mcu_init();
+int main(void) {
+    mcu_init();
 	uint8_t value;
-
-
 	/* Continuously reads analog value from channel 3 and then writes that analog value to the single DAC */
 	for(;;) {
 		value = pcf8591_analog_read(AI_CH_3);
@@ -39,4 +20,11 @@ int main(void)
 		printf("Value: %d\n", value);
 		_delay_ms(1000);
 	}
+}
+void mcu_init(void) {
+	usart_initialize();
+	stdout = &uart_output;
+	_delay_ms(10);
+	pcf8591_init();
+	printf("PCF8591 initialized.....\n");
 }
